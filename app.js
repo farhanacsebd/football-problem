b743b23bb7f21a806168a7304e72f6785a723338
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-15 12:45:59 +0600

    plus button increase

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('hello');
+document.getElementById('mars-plus').addEventListener('click',function(){
+    const marsInput = document.getElementById('mars-input');
+    const marsNumber = parseInt(marsInput.value)+1;
+    marsInput.value = marsNumber;
+})
