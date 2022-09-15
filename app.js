e2fc51b5d12e1ddae28887b59bc31adbf9653803
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-15 13:07:42 +0600

    increase total cost mars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function getInputField(increase){
+
     const marsInput = document.getElementById('mars-input');
     let marsNumber = marsInput.value;
     if(increase){
@@ -8,6 +9,13 @@ function getInputField(increase){
         marsNumber = parseInt(marsInput.value) - 1;
     }
     marsInput.value = marsNumber;
+
+
+    const marsCost = document.getElementById('mars-cost');
+    let marsCostText = parseInt(marsCost.innerText);
+    marsCostText = marsNumber*1200; 
+    marsCost.innerText = marsCostText;
+    
 }
 
 document.getElementById('mars-plus').addEventListener('click',function(){
