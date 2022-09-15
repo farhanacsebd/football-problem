a2e8ab513c6db9ab9e6e4be23e02a6837f74ba71
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-15 12:53:12 +0600

    plus or minus both in one function

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-document.getElementById('mars-plus').addEventListener('click',function(){
+function getInputField(increase){
     const marsInput = document.getElementById('mars-input');
-    const marsNumber = parseInt(marsInput.value) + 1;
+    let marsNumber = marsInput.value;
+    if(increase){
+         marsNumber = parseInt(marsInput.value) + 1;
+    }
+    else if(marsNumber>0){
+        marsNumber = parseInt(marsInput.value) - 1;
+    }
     marsInput.value = marsNumber;
+}
+
+document.getElementById('mars-plus').addEventListener('click',function(){
+    /* const marsInput = document.getElementById('mars-input');
+    const marsNumber = parseInt(marsInput.value) + 1;
+    marsInput.value = marsNumber; */
+    getInputField(true)
 })
 document.getElementById('mars-minus').addEventListener('click',function(){
-    const marsInput = document.getElementById('mars-input');
+   /*  const marsInput = document.getElementById('mars-input');
     const marsNumber = parseInt(marsInput.value) - 1;
-    marsInput.value = marsNumber;
+    marsInput.value = marsNumber; */
+    getInputField(false)
 })
